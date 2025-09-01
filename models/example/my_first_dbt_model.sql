2232b26232bf3a50c3b9bade2a942eab8a1b1171
Siri Madapa <Sirim@cloudeqs.com>
2025-09-01 08:34:51 +0000

    commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,19 +9,8 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
 select *
-from source_data
+from {{ ref('stg_source_data_ephemeral') }}
+where id is not null
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
 
- where id is not null
